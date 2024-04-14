8e5569d4fdac6fc9b3411a685f770ed380fac5e6
Jonny Saykosy <josayko@pm.me>
2024-04-14 16:03:01 +0200

    TP-05 question 1_3 1_4
    implements Ensemble methods. question1 done.

--- 05-delegation-collections/src/main/java/question1/Ensemble.java
+++ 05-delegation-collections/src/main/java/question1/Ensemble.java
@@ -16,27 +16,38 @@ public class Ensemble<T> extends AbstractSet<T> {
 
   public boolean add(T t) {
     // À compléter. Cf question 1.1 
-    return !table.contains(t) ? table.add(t) : false;
+    return !this.contains(t) ? table.add(t) : false;
   }
 
   public Ensemble<T> union(Ensemble<? extends T> e) {
     // à compléter pour la question1-2
-    return null; 
+    Ensemble<T> union = new Ensemble<>();
+    union.addAll(this);
+    union.addAll(e);
+    return union;
   }
 
   public Ensemble<T> inter(Ensemble<? extends T> e) {
     // à compléter pour la question1-2
-    return null; 
+    Ensemble<T> inter = new Ensemble<>();
+    inter.addAll(this);
+    inter.retainAll(e);
+    return inter; 
   }
 
   public Ensemble<T> diff(Ensemble<? extends T> e) {
     // à compléter pour la question1-2
-    return null; 
+    Ensemble<T> diff = new Ensemble<>();
+    diff.addAll(this);
+    diff.removeAll(inter(e));
+    return diff; 
   }
 
   public Ensemble<T> diffSym(Ensemble<? extends T> e) {
     // à compléter pour la question1-2
-    return null; 
+    Ensemble<T> union = union(e);
+    Ensemble<T> inter = inter(e);
+    return union.diff(inter);
   }
 
 
--- 05-delegation-collections/src/test/java/question1/EnsembleTest.java
+++ 05-delegation-collections/src/test/java/question1/EnsembleTest.java
@@ -20,7 +20,49 @@ public class EnsembleTest {
     e2.add(4);
     e2.add(3);
     question1.Ensemble<Integer> union = e1.union(e2);
-    // À compléter. 
+    assertArrayEquals(new Integer[]{2,3,4}, union.toArray());
+  }
+
+  @Test
+  public void test_Ensemble_Inter() { // {2,3,4} inter {5,4,3}={3,4}
+    question1.Ensemble<Integer> e1 = new Ensemble<>();
+    e1.add(2);
+    e1.add(3);
+    e1.add(4);
+    question1.Ensemble<Integer> e2 = new Ensemble<>();
+    e2.add(5);
+    e2.add(4);
+    e2.add(3);
+    question1.Ensemble<Integer> inter = e1.inter(e2);
+    assertArrayEquals(new Integer[]{3,4}, inter.toArray());
+  }
+
+  @Test
+  public void test_Ensemble_Diff() { // {1,2,3,4} diff {5,4,3}={1,2}
+    question1.Ensemble<Integer> e1 = new Ensemble<>();
+    e1.add(1);
+    e1.add(2);
+    e1.add(3);
+    question1.Ensemble<Integer> e2 = new Ensemble<>();
+    e2.add(5);
+    e2.add(4);
+    e2.add(3);
+    question1.Ensemble<Integer> diff = e1.diff(e2);
+    assertArrayEquals(new Integer[]{1,2}, diff.toArray());
+  }
+
+  @Test
+  public void test_Ensemble_DiffSym() { // {2,3,4} diffSym {5,4,3}={2,5}
+    question1.Ensemble<Integer> e1 = new Ensemble<>();
+    e1.add(2);
+    e1.add(3);
+    e1.add(4);
+    question1.Ensemble<Integer> e2 = new Ensemble<>();
+    e2.add(5);
+    e2.add(4);
+    e2.add(3);
+    question1.Ensemble<Integer> diffSym = e1.diffSym(e2);
+    assertArrayEquals(new Integer[]{2,5}, diffSym.toArray());
   }
 
   // À compléter. 
